1e720fbb5d9b42e377a4ed2c48abde1164682cce
Louis Jenkins <louisjenkinscs@dhcp-10-4-3-113.wireless.rochester.edu>
2019-04-08 12:59:52 -0400

    Skeleton updated

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -2,8 +2,27 @@
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/ScalarEvolution.h"
+#include "llvm/Analysis/ScalarEvolutionExpressions.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 using namespace llvm;
+
+// Determine if instruction I holds Induction Variable for loop L
+static bool isSimpleIVUser(Instruction *I, const Loop *L, ScalarEvolution *SE) {
+   if (!SE->isSCEVable(I->getType()))
+     return false;
+
+   // Get the symbolic expression for this instruction.
+   const SCEV *S = SE->getSCEV(I);
+
+   // Only consider affine recurrences.
+   const SCEVAddRecExpr *AR = dyn_cast<SCEVAddRecExpr>(S);
+   if (AR && AR->getLoop() == L)
+     return true;
+
+   return false;
+ }
 
 namespace {
   struct SkeletonPass : public FunctionPass {
@@ -12,8 +31,25 @@ namespace {
 
     virtual bool runOnFunction(Function &F) {
       errs() << "I saw a function called " << F.getName() << "!\n";
-      
+      LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+      ScalarEvolution &SCE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
+
+      for (Loop *loop : LI) {
+        for (BasicBlock *block : loop->getBlocks()) {
+          for (Instruction &instr : *block) {
+            if (isSimpleIVUser(&instr, loop, &SCE)) {
+              errs() << "Found Induction Variable: " << instr << "\n";
+            }
+          }
+        }
+      }
       return false;
+    }
+
+    void getAnalysisUsage(AnalysisUsage &AU) const {
+      AU.setPreservesCFG();
+      AU.addRequired<LoopInfoWrapperPass>();
+      AU.addRequired<ScalarEvolutionWrapperPass>();
     }
   };
 }

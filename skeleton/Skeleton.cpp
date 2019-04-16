d1e3ebe64309ea8dcc67a2e007cc28c55004e663
Yiming Gan <ygan10@cycle1.csug.rochester.edu>
2019-04-16 11:34:29 -0400

    all instruction and its operands

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -117,18 +117,30 @@ namespace {
             for (Loop *loop : LI) {
                 for (BasicBlock *block : loop->getBlocks()) {
                     for (Instruction &instr : *block) {
+			errs() << instr.getOpcodeName() << ":";
+			unsigned int op_cnt = instr.getNumOperands();
+			unsigned int i;
+			for (i=0; i< op_cnt; i++)
+			{
+				Value *opnd = instr.getOperand(i);
+				if (opnd->hasName())
+				{
+					errs() << opnd->getName() << ",";
+				}
+				else errs() << "ptr " << opnd << ",";
+			}
 			//if it's a store instruction
 			if (instr.getOpcode() == Instruction::Store)
 			{
-				errs() << instr << "\n";
+				unsigned int i=0;
 			}
 			//if it's a load instruction 
 			if (instr.getOpcode() == Instruction::Load)
 			{
-				errs() << instr << "\n";
-			}
-			//errs() << instr << "     " << instr.getOpcode() << "\n";
-                        
+				unsigned int i=0;
+			}        
+
+			errs() << "\n";
                     }
                 }
             }
